732ccf4a304854463d51bb92cf7df6a29f5344c3
dantaylorseo <dantaylorseo@users.noreply.github.com>
2024-11-06 16:58:51 +0000

    Fix styling

--- src/MaytapiChannel.php
+++ src/MaytapiChannel.php
@@ -21,7 +21,7 @@ class MaytapiChannel
     {
         // @phpstan-ignore method.notFound
         $message = $notification->toWhatsApp($notifiable);
-        Log::info('Sending WhatsApp message: ' . $message);
+        Log::info('Sending WhatsApp message: '.$message);
         $this->service->send($message);
     }
 }
